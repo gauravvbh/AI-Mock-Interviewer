647a780649048caef64d0338417d5407309edf0d
Gaurav <gauravrajahela884@gmail.com>
2025-03-31 01:04:07 +0530

    Fixed auth route check in LayoutWrapper

--- src/app/_components/LayoutWrapper.jsx
+++ src/app/_components/LayoutWrapper.jsx
@@ -5,11 +5,13 @@ import Header from "../dashboard/_components/Header";
 
 export default function LayoutWrapper({ children }) {
     const pathname = usePathname();
-    const authRoutes = ["/sign-in", "/sign-up"];
+    // const authRoutes = ["/sign-in", "/sign-up"];
+    // const isAuthPage = authRoutes.some(route => pathname.startsWith(route));
+    const isAuthPage = pathname.startsWith("/sign-in") || pathname.startsWith("/sign-up");
 
     return (
         <>
-            {!authRoutes.includes(pathname) && <Header />}
+            {!isAuthPage && <Header />}
             {children}
         </>
     );
